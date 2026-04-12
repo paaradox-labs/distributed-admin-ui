83f5a08b7b27ae1ad1ed353e884584ff29621e37
Mr. Paradox <avyas8927@gmail.com>
2026-04-13 01:29:01 +0530

    add: router

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>Hello from MERN Space</h1>;
-}
-
-export default App;
---
+++ src/pages/Categories.tsx
@@ -0,0 +1,5 @@
+function Categories() {
+  return <h1>Categories page</h1>;
+}
+
+export default Categories;
