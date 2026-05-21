585dd3c253c462c2d89cb7a0e586d9fbdfe72a38
Mr. Paradox <avyas8927@gmail.com>
2026-05-21 17:22:27 +0530

    add: user form fields

--- src/pages/users/forms/UserForm.tsx
+++ src/pages/users/forms/UserForm.tsx
@@ -1,12 +1,30 @@
-import { Card, Col, Form, Input, Row } from "antd"
+import { useQuery } from "@tanstack/react-query"
+import { Card, Col, Form, Input, Row, Select, Space } from "antd"
+import { getTenants } from "../../../http/api"
+import type { Tenant } from "../../../types/types"
 
 
 const UserForm = () => {
+
+     const {
+        data: tenants,
+    } = useQuery({
+        queryKey: ["tenants"],
+        queryFn: () => {
+            return getTenants().then((res) => res.data)
+        }
+    })
+
   return (
     <Row>
         <Col span={24}>
+            <Space
+            orientation="vertical"
+            size={`large`}
+            >
             <Card
-            title="Basic Info"
+            title="Basic info"
+            variant="borderless"
             >
                 <Row
                 gutter={20}
@@ -16,7 +34,7 @@ const UserForm = () => {
                 label = "First name"
                 name = "firstName"
                 >
-                <Input placeholder="John" />
+                <Input placeholder="John" size="large" />
                 </Form.Item>
                     </Col>
                                        <Col span={12}>
@@ -24,11 +42,82 @@ const UserForm = () => {
                 label = "Last name"
                 name = "lastName"
                 >
-                <Input placeholder="Doe" />
+                <Input placeholder="Doe" size="large" />
+                </Form.Item>
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item
+                label = "Email"
+                name = "email"
+                >
+                <Input placeholder="example@mail.com" size="large" /> 
                 </Form.Item>
                     </Col>
                 </Row>
             </Card>
+            <Card
+            title="Security info"
+            variant="borderless"
+            >
+                <Row
+                gutter={20}
+                >
+                    <Col span={12}>
+                <Form.Item
+                label = "Password"
+                name = "password"
+                >
+                    <Input placeholder="password" size="large" type={`password`} />
+                </Form.Item>
+                    </Col>
+                </Row>
+            </Card>
+            <Card
+            title="Role "
+            variant="borderless"
+            >
+                <Row
+                gutter={20}
+                >
+                <Col span={12}>
+                <Form.Item
+                label = "Role"
+                name = "role"
+                >  
+                        <Select
+                        size="large"
+                        style={{width: "100%"}}
+                        allowClear={true}
+                        onChange={() => {}}
+                        placeholder={`Select role`}
+                        options={tenants?.map((tenant: Tenant) => ({
+                            value: tenant.id,
+                            label: tenant.name
+                        }))}
+                        />
+                    </Form.Item>
+                </Col>
+                 <Col span={12}>
+                <Form.Item
+                label = "Restaurant"
+                name = "tenantId"
+                >  
+                        <Select
+                        size="large"
+                        style={{width: "100%"}}
+                        allowClear={true}
+                        onChange={() => {}}
+                        placeholder={`Select restaurant`}
+                        options={tenants?.map((tenant: Tenant) => ({
+                            value: tenant.id,
+                            label: tenant.name
+                        }))}
+                        />
+                    </Form.Item>
+                </Col>
+                </Row>
+            </Card>
+            </Space>
         </Col>
     </Row>
   )
